2a6f9aa47199da58e79fbfb0bb085fe9351a179f
林奇 <tlqtangok@126.com>
2025-04-24 17:00:47 +0800

    clear_proj adaptive use basename

--- t0/clear_proj.bat
+++ t0/clear_proj.bat
@@ -1,10 +1,17 @@
 :: change config to Debug, only for Debug
 @echo off
-dir /s /b /AD |grepw /V _3rd | grepw "t0$ .vs$ x64$ .vscode$" > txt.txt 
+
+setlocal
+cd |repl "^.*\\" ""  > %tmp%\basename.txt
+
+set projname=<%tmp%\basename.txt
+
+dir /s /b /AD |grepw /V _3rd | grepw "%projname%$ .vs$ x64$ .vscode$" > txt.txt 
 dir /s /b *.pdb >> txt.txt
 
 
 type txt.txt | tol | repl "^" "rm -rf "
 
 del txt.txt
+endlocal
 @echo on
