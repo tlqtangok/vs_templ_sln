2b697c9675be45f00095cd91058da335057fb9d2
林奇 <tlqtangok@126.com>
2025-04-25 09:14:44 +0800

    add swp tags file in rm -rf list

--- t0/clear_proj.bat
+++ t0/clear_proj.bat
@@ -7,7 +7,7 @@ cd |repl "^.*\\" ""  > %tmp%\basename.txt
 set /p projname=<%tmp%\basename.txt
 
 dir /s /b /AD |grepw /V _3rd | grepw "%projname%$ .vs$ x64$ .vscode$" > txt.txt 
-dir /s /b *.pdb >> txt.txt
+dir /a /s /b *.pdb  *.aps tags *.swp >> txt.txt
 
 
 type txt.txt | tol | repl "^" "rm -rf "
